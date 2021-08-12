67c003f52a735942aba787aec7f834fc02e4e2e0
mrcrypster <mr.crypster@gmail.com>
2021-08-12 20:15:48 +0000

    colors added

--- testy.php
+++ testy.php
@@ -3,39 +3,37 @@
 class testy {
   
   # Abstract function to be implemented in child classes
-  protected static function prepare() {
-    
-  }
+  protected static function prepare() {}
   
   # Compare expected and fact value, output results
   public static function assert($expected, $fact, $explanation) {
     # printf "Default color \033[0;32m green color \033[0m default again \n"
-    if ( $actual === $should_be ) {
+    if ( $expected === $fact ) {
       $print_details = false;
-      echo "\033[0;32m" . 'OK:' . "\033[0m" . ' ';
+      echo "\033[0;32m" . 'OK :' . "\033[0m" . ' ';
     }
     else {
       $print_details = true;
-      echo "\033[0;31m" . 'NO:' . "\033[0m" . ' ';
+      echo "\033[0;31m" . 'ERR:' . "\033[0m" . ' ';
     }
     
     echo $explanation . "\n";
     if ( $print_details ) {
-      echo '    Expected: ' . $should_be . "\n";
-      echo '    Got: ' . $actual . "\n";
+      echo '    Expected: ' . $expected . "\n";
+      echo '    Got: ' . $fact . "\n";
     }
   } 
   
   # Run all tests
   public static function run() {
     # do preparations
-    self::prepare();
+    static::prepare();
 
     # Execute test cases    
     $methods = (new ReflectionClass('tests'))->getMethods(ReflectionMethod::IS_STATIC);
     foreach ( $methods as $method ) {
       if ( strpos($method->name, 'test_') !== 0 ) continue;
-      echo 'Running test: ' . str_replace('test_', '', $method->name) . "\n";
+      echo "\033[1;33m" . '---- ' . str_replace('test_', '', $method->name) . '() ----' . "\033[0m" . "\n";
       call_user_func(array('tests', $method->name));
       echo "\n";
     }
